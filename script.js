cc5e40a1d758af02660d01ed35e9b891a581dd41
milkovick <kristina.miljkovic18@hotmail.com>
2023-01-29 23:54:08 +0100

    add v1

---
+++ script.js
@@ -0,0 +1,328 @@
+/**  PITANJA **/
+let p1 = {
+  tekst:
+    "Koje od navedenih mesta su Turci nazivali Fićir-bajir (u prevodu „breg za razmišljanje“)?",
+  odgovori: [
+    "Avalu",
+    "Frušku goru",
+    "Kalemegdan",
+    "Petrovaradinsku tvrđavu",
+    "Kosmaj",
+  ],
+  indexTacnogOdgovora: 2,
+};
+
+let p2 = {
+  tekst: "U Srbiji postoji most na suvom. U kom gradu se ovaj most nalazi?",
+  odgovori: ["Zemunu", "Novom Sadu", "Paraćinu", "Zrenjaninu", "Kruševcu"],
+  indexTacnogOdgovora: 3,
+};
+
+let p3 = {
+  tekst:
+    "Ulica Generala Drapšina je 22. na listi najlepših ulica sveta, a nalazi se u kom našem gradu?",
+  odgovori: ["Novom Sadu", "Subotici", "Beogradu", "Kikindi", "Loznici"],
+  indexTacnogOdgovora: 3,
+};
+
+let p4 = {
+  tekst: "Kompleks Felix Romuliana izgradio je?",
+  odgovori: [
+    "Aleksandar Makedonski",
+    "Gaj Aurelije Valerije Dioklecijan",
+    "Publije Elije Trajan Hadrijan",
+    "Marko Ulpije Nerva Trajan",
+    "Gaj Galerije Valerije Maksimilijan",
+  ],
+  indexTacnogOdgovora: 4,
+};
+
+let p5 = {
+  tekst: "Izbacite uljeza:",
+  odgovori: ["Fruška gora", "Tara", "Stara planina", "Kopaonik", "Šar-planina"],
+  indexTacnogOdgovora: 2,
+};
+
+let p6 = {
+  tekst:
+    "Skulptura „Carigradski tetrarsi“ izrađena od porfira nalazi se u kom gradu?",
+  odgovori: ["Istanbulu", "Nišu", "Veneciji", "Rimu", "Splitu"],
+  indexTacnogOdgovora: 2,
+};
+
+let p7 = {
+  tekst: "Na kojoj tvrđavi se nalazi Defterdareva kapija?",
+  odgovori: [
+    "Golubačkoj",
+    "Niškoj",
+    "Petrovaradinskoj",
+    "Beogradskoj",
+    "Smederevskoj",
+  ],
+  indexTacnogOdgovora: 3,
+};
+
+let p8 = {
+  tekst: "Martindalov test služi za merenje:",
+  odgovori: [
+    "Rastegljivosti gume",
+    "Tvrdoće metala",
+    "Otpornosti tkanine na habanje",
+    "Izdržljivosti predmeta od ratana",
+    "Tvrdoće drveta",
+  ],
+  indexTacnogOdgovora: 2,
+};
+
+let p9 = {
+  tekst: "U kom gradu se nalazi Dioklecijanova palata?",
+  odgovori: ["Dubrovniku", "Baru", "Kotoru", "Zadru", "Splitu"],
+  indexTacnogOdgovora: 4,
+};
+
+let p10 = {
+  tekst:
+    "Ulica Lombard duga samo 180 metara sa čak 8 krivina nalazi se u kom gradu?",
+  odgovori: [
+    "Majamiju",
+    "San Dijegu",
+    "San Francisku",
+    "Sakramentu",
+    "Filadelfiji",
+  ],
+  indexTacnogOdgovora: 2,
+};
+
+let p11 = {
+  tekst:
+    "Kog je porekla bio književnik i autor popularne „Milenijum“ trilogije po kojoj su snimljeni istoimeni filmovi?",
+  odgovori: ["Nemačkog", "Finskog", "Ruskog", "Švedskog", "Norveškog"],
+  indexTacnogOdgovora: 3,
+};
+
+let p12 = {
+  tekst: "Stanica Zoo nalazi se u kom nemačkom gradu?",
+  odgovori: ["Hamburgu", "Kelnu", "Berlinu", "Frankfurtu", "Dizeldorfu"],
+  indexTacnogOdgovora: 2,
+};
+
+let p13 = {
+  tekst: "Koja od navedenih država se pre zvanične promene imena zvala Burma?",
+  odgovori: ["Šri Lanka", "Mjanmar", "Laos", "Bangladeš", "Brunej"],
+  indexTacnogOdgovora: 1,
+};
+
+let p14 = {
+  tekst:
+    "Ako želite da posetite baziliku Svetog Marka, jedan od najboljih primera vizantijske arhitekture i jednu od najpoznatijih znamenitosti ovog grada, morate otputovati u koji grad?",
+  odgovori: ["Madrid", "Rim", "Barselonu", "Milano", "Veneciju"],
+  indexTacnogOdgovora: 4,
+};
+
+let p15 = {
+  tekst:
+    "Na zastavi koje države se nalaze žuta zvezda, knjiga i puška AK-47 (Kalašnjikov)?",
+  odgovori: ["Somalije", "Severne Koreje", "Mozambika", "Kube", "Zimbabvea"],
+  indexTacnogOdgovora: 2,
+};
+
+let p16 = {
+  tekst:
+    "Koja je životinja odgovorna za više ljudskih žrtava u Africi od bilo koje druge velike životinje?",
+  odgovori: ["Hijena", "Nosorog", "Nilski konj", "Slon", "Krokodil"],
+  indexTacnogOdgovora: 2,
+};
+
+let p17 = {
+  tekst: "Gde su se prvo počele koristiti papirne novčanice?",
+  odgovori: ["Indija", "Egipat", "Portugal", "Kina", "Španija"],
+  indexTacnogOdgovora: 3,
+};
+
+let p18 = {
+  tekst:
+    "U kojoj se državi nalazi Angkor Wat, najveći hramski (religijski) kompleks na svetu?",
+  odgovori: ["Kambodža", "Vijetnam", "Tajland", "Peru", "Indija"],
+  indexTacnogOdgovora: 0,
+};
+
+let p19 = {
+  tekst: "Koja od navedenih egipatskih građevina je najstarija?",
+  odgovori: [
+    "Džoserova piramida",
+    "Keopsova piramida",
+    "Mikerinova piramida",
+    "Velika sfringa",
+    "Kefrenova piramida",
+  ],
+  indexTacnogOdgovora: 0,
+};
+
+let p20 = {
+  tekst:
+    "Pre solo karijere, Sting je bio glavni tekstopisac, pevač i basista koje rok grupe?",
+  odgovori: ["The Clash", "The Who", "The Police", "Oasis", "Duran Duran"],
+  indexTacnogOdgovora: 2,
+};
+
+let p21 = {
+  tekst: "U kom gradu se nalazi Transamerička piramida?",
+  odgovori: [
+    "Njujorku",
+    "Vašingtonu",
+    "Sijetlu",
+    "San Francisku",
+    "Los Anđelesu",
+  ],
+  indexTacnogOdgovora: 3,
+};
+
+let pitanja = [
+  p1,
+  p2,
+  p3,
+  p4,
+  p5,
+  p6,
+  p7,
+  p8,
+  p9,
+  p10,
+  p11,
+  p12,
+  p13,
+  p14,
+  p15,
+  p16,
+  p17,
+  p18,
+  p19,
+  p20,
+  p21,
+];
+
+/** FUNKCIONALNOST **/
+let btnPosalji = document.getElementById("submit-btn");
+let btnNovaPitanja = document.getElementById("nova-pitanja-btn");
+let novaRandomPitanja;
+
+let petRandomPitanja = (pitanja) => {
+  let kopijaPitanja = [...pitanja];
+  novaRandomPitanja = [];
+  for (let i = 0; i < 5; i++) {
+    let randNum = Math.floor(Math.random() * kopijaPitanja.length);
+    let splicedPitanje = kopijaPitanja.splice(randNum, 1)[0];
+    novaRandomPitanja.push(splicedPitanje);
+  }
+  return novaRandomPitanja;
+};
+
+let sadrzajKviza = () => {
+  petRandomPitanja(pitanja);
+  for (let i = 0; i < novaRandomPitanja.length; i++) {
+    let pitanjeParagr = document.createElement("p");
+    let divPitanjeOdgovor = document.createElement("div");
+    let mainSekcija = document.querySelector(".main-section");
+    pitanjeParagr.textContent = `${i + 1}) ${novaRandomPitanja[i].tekst}`;
+    divPitanjeOdgovor.append(pitanjeParagr);
+    mainSekcija.append(divPitanjeOdgovor);
+
+    for (let n = 0; n < novaRandomPitanja[i].odgovori.length; n++) {
+      let divOdg = document.createElement("div");
+      let labelaOdg = document.createElement("label");
+      let radioOdg = document.createElement("input");
+      radioOdg.type = "radio";
+      radioOdg.name = `p${i + 1}`;
+      radioOdg.value = novaRandomPitanja[i].odgovori[n];
+      labelaOdg.for = novaRandomPitanja[i].odgovori[n];
+      radioOdg.id = novaRandomPitanja[i].odgovori[n];
+      labelaOdg.innerHTML = radioOdg.value;
+      divOdg.append(radioOdg, labelaOdg);
+      divOdg.classList.add("div-odgovori-flex");
+      divPitanjeOdgovor.classList.add("pitanje-odgovor-div");
+      divPitanjeOdgovor.append(divOdg);
+      let prviRadioBtnCheck = document.querySelectorAll(
+        ".pitanje-odgovor-div .div-odgovori-flex:nth-child(2) input[type='radio']"
+      );
+      prviRadioBtnCheck.forEach((rbtn) => {
+        rbtn.checked = "checked";
+      });
+    }
+  }
+};
+sadrzajKviza();
+
+// Blokiranje izmene radio buttons-a nakon submita
+let blokRadioBtns = () => {
+  let blockBtns = document.querySelectorAll(
+    ".pitanje-odgovor-div .div-odgovori-flex input[type='radio']"
+  );
+  for (let i = 0; i < blockBtns.length; i++) {
+    blockBtns[i].disabled = true;
+  }
+};
+
+// Blokiranje submit buttona nakon submitovanih odgovora
+btnPosalji.addEventListener("click", (e) => {
+  setTimeout(() => (e.target.disabled = true), 0);
+});
+
+let odgovoriKorisnika = () => {
+  let odgovorKorisnika = document.querySelectorAll(
+    ".pitanje-odgovor-div input[type='radio']:checked"
+  );
+  for (let i = 0; i < novaRandomPitanja.length; i++) {
+    let paragrafTacno;
+    let paragrafNetacno;
+    if (
+      novaRandomPitanja[i].odgovori.indexOf(odgovorKorisnika[i].value) ==
+      novaRandomPitanja[i].indexTacnogOdgovora
+    ) {
+      paragrafTacno = document.createElement("p");
+      document.body.append(paragrafTacno);
+      paragrafTacno.textContent = `Tačno ste odgovorili na ${i + 1}. pitanje`;
+      paragrafTacno.classList.add("paragraf-tacno");
+    } else {
+      paragrafNetacno = document.createElement("p");
+      document.body.append(paragrafNetacno);
+      paragrafNetacno.textContent = `Niste tačno odgovorili na ${
+        i + 1
+      }. pitanje`;
+      paragrafNetacno.classList.add("paragraf-netacno");
+    }
+    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+  }
+  blokRadioBtns();
+};
+
+let promenaPozadineDivPitanja = () => {
+  let sekcijaOdgovori = document.querySelectorAll("body > p");
+  let divTacanOdg = document.querySelectorAll(".pitanje-odgovor-div");
+  for (let i = 0; i < sekcijaOdgovori.length; i++) {
+    if (sekcijaOdgovori[i].classList.contains("paragraf-tacno")) {
+      divTacanOdg[i].classList.add("pozadina-tacno");
+    } else if (sekcijaOdgovori[i].classList.contains("paragraf-netacno")) {
+      divTacanOdg[i].classList.add("pozadina-netacno");
+    }
+  }
+};
+
+// Reload stranice za nova pitanja
+let novaPitanjaReloadKviz = () => {
+  window.location.reload(1);
+};
+
+// Rezultati kviza
+btnPosalji.addEventListener("click", () => {
+  odgovoriKorisnika();
+  promenaPozadineDivPitanja();
+});
+
+// Generisanje novih pitanja
+btnNovaPitanja.addEventListener("click", () => {
+  document.querySelector("body").scrollIntoView({
+    block: "start",
+  });
+  petRandomPitanja(pitanja);
+  novaPitanjaReloadKviz();
+});
